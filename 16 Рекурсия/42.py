12b98066d430c2de6b1c90842387002128d8a4e5
SupremeW1zard24 <172936104+SupremeW1zard24@users.noreply.github.com>
2024-12-08 21:09:32 +0500

    Create 42.py

---
+++ 16 Рекурсия/42.py
@@ -0,0 +1,18 @@
+cache = {}
+
+
+def f(n):
+    if n not in cache:
+        if n == 1: cache[n] = 1
+        else: cache[n] = f(n - 1) + 3 * g(n - 1)
+
+    return cache[n]
+
+
+def g(n):
+    if n == 1: return 1
+    if n > 1: return f(n - 1) - 2 * g(n - 1)
+
+
+k = f(50)
+print(sum(int(el) for el in str(k)))
