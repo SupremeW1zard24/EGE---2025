2a680138062b1481fee68e87be6a9898461381b6
SupremeW1zard24 <172936104+SupremeW1zard24@users.noreply.github.com>
2024-12-08 21:15:51 +0500

    Update 42.py

--- 16 Рекурсия/42.py
+++ 16 Рекурсия/42.py
@@ -16,3 +16,23 @@ def g(n):
 
 k = f(50)
 print(sum(int(el) for el in str(k)))
+
+# ------------------------   РЕШЕНИЕ С ИСПОЛЬЗОВАНИЕМ ВСТРОЕННОЙ БИБЛИОТЕКИ   ----------------------------
+
+from functools import *
+
+
+@lru_cache(None)
+def f(n):
+    if n == 1: return 1
+    if n > 1: return f(n - 1) + 3 * g(n - 1)
+
+
+def g(n):
+    if n == 1: return 1
+    if n > 1: return f(n - 1) - 2 * g(n - 1)
+
+
+k = f(50)
+print(sum(int(el) for el in str(k)))
+
