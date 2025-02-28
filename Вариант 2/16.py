3f63f9638ccdd73c00bfffb8c9968fe3db322452
SupremeW1zard24 <2008-Arsen-2008@mail.ru>
2025-02-28 23:30:51 +0500

    Create 16.py

---
+++ Вариант 2/16.py
@@ -0,0 +1,15 @@
+from functools import *
+
+
+@lru_cache(None)
+def f(n):
+    if n < 5:
+        return 4 ** 4
+    if n > 4:
+        return 4 * f(n - 4) + 4
+
+
+for i in range(1, 5000):
+    f(i)
+
+print(f(4048) / f(4036))
